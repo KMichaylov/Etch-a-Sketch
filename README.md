e8876a42f8d154876c4fec848034f86945fcc8dc
Kristiyan <kristiyan@propeld.nl>
2022-10-26 20:02:53 +0200

    Finished JS functionality for creating cells and added a README.md file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Etch-a-Sketch project
+## Information about the assignment here: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let grid = document.getElementById('grid-container');
-let inputFromUser = 2
-// Draw the grid and place them in grid
-for(let i = 0; i < inputFromUser; i++) {
-    for (let j = 0; j < inputFromUser; j++) {
-        let square = grid.appendChild(document.createElement('div'));
-        square.style.border = 'medium solid #0000FF';
-        square.style.display = 'flex';
-        square.style.flexDirection = 'row'      
-    }
-}
+let grid = document.getElementById("grid-container");
+let inputFromUser = 25;
+const CANVAS_SIZE = 500;
+const CELL_SIZE = 500 / inputFromUser;
+const CELL_SIZEINPX = new String(CELL_SIZE + "px");
 
+// Draw the grid and place them in grid
+for (let i = 0; i < inputFromUser; i++) {
+  let squareOnColumn = grid.appendChild(document.createElement("div"));
+  squareOnColumn.className = "column";
+  squareOnColumn.style.width = CELL_SIZEINPX;
+  squareOnColumn.style.height = CELL_SIZEINPX;
+  for (let j = 0; j < inputFromUser; j++) {
+    let squareOnRow = grid.appendChild(document.createElement("div"));
+    squareOnRow.className = "row";
+    squareOnRow.style.width = CELL_SIZEINPX;
+    squareOnRow.style.height = CELL_SIZEINPX;
+    squareOnColumn.appendChild(squareOnRow);
+  }
+  grid.appendChild(squareOnColumn);
+}
